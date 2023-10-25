7a6c8a677155f0f83e92798f8828f01866cb6cc7
rvDOOM <rvalerio9131@gmail.com>
2023-10-25 16:51:19 -0400

    Added custom login and signup functionality

---
+++ user/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")  # Redirect to the homepage
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("index")  # Redirect to the homepage
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
