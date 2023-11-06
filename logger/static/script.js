9389299f96c629ac9ad5dcfb4458709829173a00
rvDOOM <rvalerio9131@gmail.com>
2023-11-06 15:34:16 -0500

    Cleaned up some logic for displaying workouts

--- logger/static/script.js
+++ logger/static/script.js
@@ -1,61 +1,108 @@
 // Document Elements
 
-const newWorkoutElement = document.querySelector('.js-add-workout');
+const addWorkoutElement = document.querySelector('.js-add-workout');
 const setFormElement = document.querySelector('.js-workout-display');
 
 
 // Add new workout
 
 let currWorkouts = [];
-newWorkoutElement.addEventListener('click',() => {
+addWorkoutElement.addEventListener('click',() => {
   let newWorkoutObject = {
     name: '',
     sets: [],
-    "init-setup": true,
-
+    "init-setup": true 
   }
-
-  let confirmName = 
-    `<button class="js-confirm-workout" onclick="confirm(${currWorkouts.length})">confirm</button>`;
-  let workoutDisplayElement = document.querySelector('.js-workout-display');
-
-  workoutDisplayElement.innerHTML +=
-    `<div class="js-workout-display-${currWorkouts.length}">
-    <div class='workout-number'>Workout ${currWorkouts.length + 1}</div>
-    <div class='js-workout-name-${currWorkouts.length}'>
-      <input type="text" placeholder="" class="js-workout-input-${currWorkouts.length}">${confirmName}
-    </div>
-    <div class='js-set-display-${currWorkouts.length}'></div>
-    </div>`;
-
-    
   currWorkouts.push(newWorkoutObject);
-
+  displayManager();
 })
 
-const confirm = (index) => {
-  let currInputNum = ".js-workout-input-" + index;
-  let currWorkoutName = ".js-workout-name-" + index;
-  let currInput = document.querySelector(currInputNum);
-  let content;
-  currWorkoutName = document.querySelector(currWorkoutName);
-  if (!currInput.value && currInput.placeholder){
-    content = currInput.placeholder;
+
+function displayManager() {
+
+  let workoutDisplayElement = document.querySelector('.js-workout-display');
+
+  workoutDisplayElement.innerHTML = '';
+  currWorkouts.forEach((object, index) => {
+
+  if(object['init-setup'] === true) {
+    let confirmName = 
+      `<button class="js-confirm-workout" onclick="confirm(${index})">confirm</button>`;
+
+    workoutDisplayElement.innerHTML +=
+      `<div class="js-workout-display-${index}">
+      <div class='workout-number'>Workout ${index + 1}</div>
+      <div class='js-workout-name-${index}'>
+        <input type="text" placeholder="" class="js-workout-input-${index}">${confirmName}
+      </div>
+      </div>`;
   } else {
-    content = currInput.value;
+    workoutDisplayElement.innerHTML +=
+      `<div class="js-workout-display-${index}">
+      <div class='workout-number'>Workout ${index + 1}</div>
+      <div class='js-workout-name-${index}'>
+        Name: ${object.name}
+        <button class='js-edit-button-${index}' onclick="editWorkoutName(${index})">
+        Edit
+        </button>
+      </div>
+      <div class='js-set-display-${index}'></div>
+      <div class='js-add-new-set-div-${index}'>
+        <button class='js-add-new-set-button-${index}' onclick='addNewSet(${index})'>Add Set</button>
+      </div>
+      </div>`;
+      
+      let setDisplayElement = document.querySelector('.js-set-display-' + index)
+      setDisplayElement.innerHTML = renderSets(index);
   };
+
+  })
+
+  function renderSets(index) {
+    let renderedHtml = '';
+
+    currWorkouts[index].sets.forEach((object, idx) => {
+    renderedHtml += `<div>Set: ${idx + 1} <input type='text' class='js-set-weight-${index}' placeholder='${object.weight}'>
+    <input type='text' class='js-set-reps-${index}' placeholder='${object.reps}'></div>`;
+
+    })
+
+
+    return renderedHtml;
+  }
+
+
+}
+
+const confirm = (index) => {
+
+
+  let currInputElement = ".js-workout-input-" + index;
+  let currWorkoutNameElement = ".js-workout-name-" + index;
+  currInputElement = document.querySelector(currInputElement);
+  currWorkoutNameElement = document.querySelector(currWorkoutNameElement);
+
+  let content;
+  if (!currInputElement.value && currInputElement.placeholder){
+    content = currInputElement.placeholder;
+  } else {
+    content = currInputElement.value;
+  };
+
   currWorkouts[index].name = content;
-  console.log(currWorkouts[index].name);
 
-  let editButton = `<button class='js-edit-button-${index}' onclick="editWorkoutName(${index})">Edit</button>`;
+  currWorkouts[index]['init-setup'] = false;
+  displayManager();
+};
 
-  currWorkoutName.innerHTML = `Name: ${content}` + editButton;
+const editWorkoutName = (index) => {
+  let currWorkoutElement = ".js-workout-name-" + index;
+  currWorkoutElement = document.querySelector(currWorkoutElement);
+  currWorkoutElement.innerHTML= 
+    `Name: <input type='text' class="js-workout-input-${index}" placeholder='${currWorkouts[index].name}'>
+    <button onclick='confirm(${index})'>Confirm</button>`;
 
-  if(currWorkouts[index]["init-setup"] === true){
-    console.log("We in true");
-    addNewSet(index);
-    currWorkouts[index]["init-setup"] = false;
-  };
+  console.log("Element " + index + " edit");
 }
 
 const addNewSet = (index) => {
@@ -80,36 +127,3 @@ const addNewSet = (index) => {
   currWorkoutDisplayElement.innerHTML = finalHtml;
 }
 
-const editWorkoutName = (index) => {
-  let currWorkoutElement = ".js-workout-name-" + index;
-  currWorkoutElement = document.querySelector(currWorkoutElement);
-  currWorkoutElement.innerHTML= 
-    `Name: <input type='text' class="js-workout-input-${index}" placeholder='${currWorkouts[index].name}'>
-    <button onclick='confirm(${index})'>Confirm</button>`;
-
-  console.log("Element " + index + " edit");
-}
-
-
-
-
-const displayWorkouts =  () => {
-  setFormElement.innerHTML = '';
-  currWorkouts.forEach((object, index) => {
-
-    console.log(index);
-    setFormElement.innerHTML += object.html;
-
-
-    if (currWorkouts[index]["init-setup"]) {
-    } else {
-
-      // TODO
-      // Need to the logic for workouts that currently exist
-      // Currently once Add Workout is pressed display breaks
-
-
-    }
-    console.log(object);
-  })
-}
